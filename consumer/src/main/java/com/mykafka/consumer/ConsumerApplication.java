a27350e3db4f1745423564ee59be3279f065baae
Marinko Babic <marinko.babic@crealogix.com>
2018-08-13 23:18:24 +0200

    improve the example add the docker-compose file in order to start kafka properly

--- consumer/src/main/java/com/mykafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/mykafka/consumer/ConsumerApplication.java
@@ -1,24 +1,14 @@
 package com.mykafka.consumer;
 
-import java.util.concurrent.CountDownLatch;
-
-import org.axonframework.spring.config.EnableAxon;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
-public class ConsumerApplication implements CommandLineRunner {
+public class ConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
-	private CountDownLatch latch = new CountDownLatch(1);
-
-	@Override
-	public void run(String... args) throws Exception {
-		latch.await();
-	}
 }
--- consumer/src/main/java/com/mykafka/consumer/MyConfig.java
+++ consumer/src/main/java/com/mykafka/consumer/MyConfig.java
@@ -3,17 +3,10 @@
  */
 package com.mykafka.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.axonframework.boot.autoconfig.AxonAutoConfiguration;
-import org.axonframework.config.EventHandlingConfiguration;
-import org.axonframework.config.EventProcessingConfiguration;
 import org.axonframework.kafka.eventhandling.DefaultKafkaMessageConverter;
 import org.axonframework.kafka.eventhandling.KafkaMessageConverter;
-import org.axonframework.kafka.eventhandling.consumer.KafkaMessageSource;
 import org.axonframework.serialization.Serializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -24,18 +17,10 @@ import org.springframework.context.annotation.Configuration;
 @AutoConfigureAfter(AxonAutoConfiguration.class)
 public class MyConfig {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(MyEventEventHandler.class);
-
   @ConditionalOnMissingBean
   @Bean
   public KafkaMessageConverter<String, byte[]> kafkaMessageConverter(
       @Qualifier("eventSerializer") Serializer eventSerializer) {
     return new DefaultKafkaMessageConverter(eventSerializer);
   }
-
-  @Autowired
-  public void configure(EventProcessingConfiguration config) {
-    config.usingTrackingProcessors(); // default all processors to tracking mode.
-  }
-
 }
